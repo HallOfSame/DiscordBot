8b53c662b7115ca424e9737e28ddbcc47a9b7453
HallOfSame <samuel.ogara94@gmail.com>
2021-02-23 18:59:07 -0500

    Add empty data store class
    This class wil eventually be used to hold info we get from the READY, GUILD_CREATE and other events.
    We can use this to be able to track info needed for connecting to voice and handling messages.

---
+++ DiscordBot/DataStore/DataStore.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordBot.DataStore
+{
+    /// <summary>
+    /// TODO add data store to hold info like Guild & Channel info that we need to reference Won't be sure what that includes until we
+    /// start doing message reading & voice connections.
+    /// </summary>
+    public class DataStore
+    {
+    }
+}
